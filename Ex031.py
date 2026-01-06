77524c3265896a2f647d341829e2238f8f2839e8
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-06 19:08:59 -0300

    Python 3 - Mundo 1
    Ex031.py

---
+++ Ex031.py
@@ -0,0 +1,6 @@
+distancia = float(input('Distância da viagem: '))
+print('-' * 12)
+if distancia <= 200:
+    print(f'Preço: R${distancia * 0.5:.2f}')
+else:
+    print(f'Preço: R${distancia * 0.45:.2f}')
