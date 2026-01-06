29b11d35bb2297730e59b06b93e1f0ea637a0fc5
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-06 19:06:49 -0300

    Python 3 - Mundo 1
    Ex027.py

---
+++ Ex027.py
@@ -0,0 +1,7 @@
+n = str(input('Digite o seu nome completo: ')).strip()
+
+nome = n.split()
+print('Muito prazer em te conhecer')
+print('Primeiro nome é {}'.format(nome[0]))
+
+print('O seu ultimo nome é {}'.format(nome[len(nome) - 1]))
