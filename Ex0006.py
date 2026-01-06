52478b6ac6cca8a70dd3938694d651c81072d524
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-06 18:48:24 -0300

    Python 3 - Mundo 1
    Ex0006.py

---
+++ Ex0006.py
@@ -0,0 +1,4 @@
+n= int(input('Digite um número: '))
+print('O dobro de {} vale {}.'.format(n, (n *2)))
+print('O triplo de {} vale {}.\n a raiz quadrada de {:.2f} é igual a {}.'.format(n, (n*3), n, pow(n, (1/2))))
+
