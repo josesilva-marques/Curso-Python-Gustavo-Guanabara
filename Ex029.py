dcc10ed30a79448ac707c8e930dffe01bd9effbd
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-06 19:07:52 -0300

    Python 3 - Mundo 1
    Ex029.py

---
+++ Ex029.py
@@ -0,0 +1,7 @@
+velocidade = int(input('Velocidade atual: '))
+print('-' * 12)
+
+if velocidade <= 80:
+    print('Mantenha sempre no limite.')
+else:
+    print(f'Você foi multado em {(velocidade - 80) * 7} reais.')
