9b3dbf96add7d439dbdea792d1bd79bc56d42ef4
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-30 15:14:33 -0300

    Python 3 - Mundo 2
    Ex038.py

---
+++ Ex038.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+
+if n1 > n2:
+    print('Maior: ',n1)
+elif n2 > n1:
+    print('Maior: ',n2)
+
+elif n1 == n2:
+    print('Numeros Iguais')
