5079138aa88e2c44f900c9c8fd86b2801457b7b9
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-30 15:55:33 -0300

    Python 3 - Mundo 2
    Ex060.py

---
+++ Ex060.py
@@ -0,0 +1,8 @@
+from math import factorial
+
+            #outra maneira de resolver, usando modulos
+
+n1 = int(input('Digite um numero : '))
+fat = factorial(n1)
+
+print('O fatorial de {} é {} '.format(n1,fat))
