589eb0d3aefeae2305d57f8650873a68a3b5610c
Jose Marques <150544429+josesilva-marques@users.noreply.github.com>
2026-01-30 15:50:43 -0300

    Python 3 - Mundo 2
    Ex053.py

---
+++ Ex053.py
@@ -0,0 +1,12 @@
+pali = input('Digite um palavra: ').upper() #coloca afrse toda em letras maiusculas
+
+frase_dividida = pali.split()#usamos o .split para separar a frase em espaços
+frase_junta = ''.join(frase_dividida)#o join sera usado para juntar as palavras q o .split separou, lembre-se o join separa as palavras pelos espaços
+
+frase_invertida = ''
+
+for cont in range(len(frase_junta) -1, -1, -1):
+    frase_invertida += frase_junta[cont]
+
+print(f'Palavra Junta: {frase_junta}')
+print(f'Palavra Invertida: {frase_invertida}')
